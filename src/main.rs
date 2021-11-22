fe514c7962f378eeb18b87a06790745f296178b5
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-11-22 11:05:09 +0900

    matching 알고리즘 추가

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
 use std::env;
 
-use serenity::prelude::*;
+use serenity::client::{Client, Context};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
 
+pub mod random_matching;
 pub mod receive_event;
 use receive_event::*;
+
+#[command]
+async fn about(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "A simple test bot").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "pong!").await?;
+
+    Ok(())
+}
+
+
+#[group]
+#[commands(about, ping)]
+struct General;
 
 #[tokio::main]
 async fn main() {
   let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-  let mut client = Client::new(token)
+  let framework = StandardFramework::new()
+                  .configure(|c| c.prefix("~"))
+                  .group(&GENERAL_GROUP);
+
+  let mut client = Client::builder(token)
     .event_handler(Handler)
+    .framework(framework)
     .await
     .expect("Err creating client");
 
